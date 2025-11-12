a6c2a5200d22fb31fd0376391bb1bc4d3c042fb6
KAMLESHWAR KODAG <gopalspayingguestservices@gmail.com>
2025-11-12 19:50:10 +0530

    changes

--- controllers/AttendanceController.js
+++ controllers/AttendanceController.js
@@ -44,6 +44,54 @@ const fetchAttendanceDetailSheetData = async (req, res) => {
   }
 };
 
+
+// const { AllSheetNames } = require("../Config");
+
+const fetchSallaryTrackerDetailSheetData = async (req, res) => {
+  const spreadsheetId = "1obwi57lOWuJvnq6dybq0_2Rs743nQqhdA_sBKmjHs88";
+    const now = new Date();
+        const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+        // const sheetTitle = `${monthNames[now.getMonth()]}${now.getFullYear()}`;
+        const sheetTitle = `ForIT-DoNotDelete`;
+
+  if (!spreadsheetId || !sheetTitle) {
+    return res.status(400).json({ success: false, message: "Missing sheet ID or title" });
+  }
+
+  try {
+    const doc = new GoogleSpreadsheet(spreadsheetId);
+
+    await doc.useServiceAccountAuth({
+      client_email: process.env.GOOGLE_CLIENT_EMAIL,
+      private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+    });
+
+    await doc.loadInfo();
+
+    const sheet = doc.sheetsByTitle[sheetTitle];
+    if (!sheet) {
+      return res.status(404).json({ success: false, message: "Sheet not found" });
+    }
+
+    const rows = await sheet.getRows();
+    const headers = sheet.headerValues.map(h => h.replace(/\n/g, ' ').trim());
+
+    const data = rows.map(row => {
+      const rowData = {};
+      headers.forEach((header, i) => {
+        rowData[header] = row._rawData[i] || "";
+      });
+      return rowData;
+    });
+
+    return res.json({ success: true, total: data.length, data });
+  } catch (error) {
+    console.error("Error fetching sheet:", error.message);
+    return res.status(500).json({ success: false, message: "Failed to fetch sheet data" });
+  }
+};
+
 module.exports = {
-  fetchAttendanceDetailSheetData,
+  fetchSallaryTrackerDetailSheetData,
+  fetchAttendanceDetailSheetData
 };
--- routes/attendanceRoutes.js
+++ routes/attendanceRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const { fetchAttendanceDetailSheetData } = require('../controllers/AttendanceController');
+const { fetchSallaryTrackerDetailSheetData } = require('../controllers/AttendanceController');
 
 // POST /add-row
 router.get('/Attendance-details', fetchAttendanceDetailSheetData);
+router.get('/sallary-tracker-details', fetchSallaryTrackerDetailSheetData);
 
 module.exports = router;
